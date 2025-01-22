57ed8482fcf6cd72f34e52b0c6b0b2cd0ec998e4
Kai Kreuzer <kai.kreuzer@telekom.de>
2025-01-22 20:43:21 +0100

    Fix the documentation "edit" button (#11)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/eclipse-lmos/website/edit/main',
+            'https://github.com/eclipse-lmos/website/edit/source',
         },
         blog: {
           showReadingTime: true,
